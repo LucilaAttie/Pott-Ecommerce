6df8aef3954b734cbedb7a021ceee6d287ec886e
Lucila Attie <luli.attie@gmail.com>
2024-02-21 23:16:20 -0300

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,95 @@
+// import { NavLink, Link } from "react-router-dom";
+// import CartWidget from "../CartWidget/CartWidget";
+// import './Navbar.css';
+
+// const Navbar = () => {
+//     return (
+//         <header className="navbar navbar-expand-lg navbar-light bg-light px-5">
+//             <div className="container-fluid">
+//                 <Link className="navbar-brand" to="/">
+//                     <img src="./logo.ico" className="logoNavbar" alt="" />
+//                     <h4>POTT.</h4>
+//                 </Link>
+
+//                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//                     <span className="navbar-toggler-icon"></span>
+//                 </button>
+
+//                 <div className="collapse navbar-collapse" id="navbarNav">
+//                     <ul className="navbar-nav ms-auto">
+//                         <li className="nav-item">
+//                             <NavLink to={`/category/tazas`} className="nav-link">Tazas</NavLink>
+//                         </li>
+//                         <li className="nav-item">
+//                             <NavLink to={`/category/bowls`} className="nav-link">Bowls</NavLink>
+//                         </li>
+//                         <li className="nav-item">
+//                             <NavLink to={`/category/floreros`} className="nav-link">Floreros</NavLink>
+//                         </li>
+//                         <li className="nav-item">
+//                             <NavLink to={`/category/platos`} className="nav-link">Platos</NavLink>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </div>
+//             <CartWidget />
+//         </header>
+//     )
+// }
+
+// export default Navbar;
+
+import { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import CartWidget from "../CartWidget/CartWidget";
 import './Navbar.css';
 
-
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <header className="navbar navbar-expand-lg bg-body-tertiary px-5">
+        <header className="navbar navbar-expand-lg navbar-light bg-light px-5">
             <div className="container-fluid">
-                <Link className="navbar-brand brandContainer" to ="/" >
-                    <img src="./logo.ico" className="logoNavbar" alt=""></img>
+                <Link className="navbar-brand" to="/">
+                    <img src="./logo.ico" className="logoNavbar" alt="" />
                     <h4>POTT.</h4>
                 </Link>
 
-                <nav>
-                    <ul className="navbar-nav mb-2 mb-lg-0">
-                        <li>
-                            <NavLink  to={`/category/tazas`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' } >Tazas</NavLink>
-                        </li>
+                <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                        <li>
-                        <NavLink to={`/category/bowls`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' } >Bowls</NavLink>
+                <div className={"collapse navbar-collapse" + (isOpen ? " show" : "")} id="navbarNav">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <NavLink to={`/category/tazas`} className="nav-link">Tazas</NavLink>
                         </li>
-
-                        <li>
-                            <NavLink  to={`/category/floreros`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' } >Floreros</NavLink>
+                        <li className="nav-item">
+                            <NavLink to={`/category/bowls`} className="nav-link">Bowls</NavLink>
                         </li>
-
-                        <li>
-                        <NavLink to={`/category/platos`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' } >Platos</NavLink>
+                        <li className="nav-item">
+                            <NavLink to={`/category/floreros`} className="nav-link">Floreros</NavLink>
                         </li>
-                        
+                        <li className="nav-item">
+                            <NavLink to={`/category/platos`} className="nav-link">Platos</NavLink>
+                        </li>
                     </ul>
-                    
-                    
-                </nav>
+                </div>
+
+                <div className="ms-auto">
+                    <CartWidget />
+                </div>
             </div>
-            
-            
-            
-            <CartWidget/>
         </header>
     )
 }
 
-export default Navbar
+export default Navbar;
+
+
+
+
